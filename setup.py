a083ec65f5878c81a151453b50d7b73f3ed5d128
Masato Tsuchiya <malk271828@gmail.com>
2018-09-23 00:29:27 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GoFPatterns",
+    version="0.1",
+    packages=find_packages(),
+    description=("Gang of Four Patterns),
+    author="Masato Tsuchiya"
+)
